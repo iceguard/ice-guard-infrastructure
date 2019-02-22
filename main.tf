476ce03421df210089d40a0da4f8e0753f71118d
Louis Leon Müller <louisleon.mueller@swisscom.com>
2019-02-22 16:18:06 +0100

    backendconfig in main deleted

--- main.tf
+++ main.tf
@@ -5,14 +5,6 @@ provider "azurerm" {
   client_secret   = "${var.client_secret}"
 }
 
-terraform{
-    backend "azurerm" {
-    storage_account_name = "${var.storage_account_name}"
-    container_name       = "${var.container_name}"
-    key                  = "${var.key}"
-    access_key           = "${var.access_key}"
-  }
-}
 
 resource "azurerm_resource_group" "igss_automation_rg" {
     name = "igss-automation-rg"
