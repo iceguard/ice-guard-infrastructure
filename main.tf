0fb67a9913f3a98b46d1d630100b9f4906cb3f58
Louis Leon Müller <louisleon.mueller@swisscom.com>
2019-02-25 09:23:26 +0100

    Terraform backend storgae account and container added.

--- main.tf
+++ main.tf
@@ -16,3 +16,29 @@ resource "azurerm_resource_group" "igss_automation_rg" {
         Environment = "${var.env_tag}"
     }
 }
+
+###################
+#Terraform Backend
+###################
+
+resource "azurerm_storage_account" "igss_terraform_sa" {
+  name                     = "igssterraformsa"
+  resource_group_name      = "${azurerm_resource_group.igss_automation_rg.name}"
+  location                 = "${azurerm_resource_group.igss_automation_rg.location}"
+  account_tier             = "Standard"
+  account_replication_type = "LRS"
+  account_kind             = "BlobStorage"
+  
+  
+  tags {
+        Project = "${var.igss_project_tag}"
+        Environment = "${var.igss_env_tag}"
+  }
+}
+
+resource "azurerm_storage_container" "igss_terraform_sa01_container01" {
+  name                  = "terraformstatefiles"
+  resource_group_name   = "${azurerm_resource_group.igss_automation_rg.name}"
+  storage_account_name  = "${azurerm_storage_account.igss_terraform_sa.name}"
+  container_access_type = "private"
+}
